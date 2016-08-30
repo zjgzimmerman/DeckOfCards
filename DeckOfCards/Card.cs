46841f9927b30f20a7b81dd96abc42dc12eccebe
Zachary Zimmerman <zjgzimmerman@gmail.com>
2016-08-30 13:41:24 -0400

    Actually add the files this time

---
+++ DeckOfCards/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class Card
+    {
+        enum suits { Clubs, Spades, Diamonds, Hearts };
+        enum faceValues { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
+
+        //Constructor, Requires a suit and face value
+        public Card()
+        {
+        }
+
+
+    }
+}
